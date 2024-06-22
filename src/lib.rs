1ade33e590ee5dfca5a48e52933dc5881a3b3d6d
Dylan Hardison <dylan@hardison.net>
2024-06-22 01:20:57 -0700

    types

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, FnArg, ItemFn, PatType, TypePath, TypeReference, TypeSlice};
+use syn::{parse_macro_input, FnArg, Item, ItemFn, ItemStruct, ItemType, PatType, TypePath, TypeReference, TypeSlice};
 
 
 struct CheckArg {
@@ -14,8 +14,6 @@ struct CheckArg {
 pub fn export(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemFn);
 
-    // let _args = parse_macro_input!(attr as AttributeArgs);
-
     let vis = &input.vis;
     let sig = &input.sig;
     let unsafety = &sig.unsafety;
@@ -24,7 +22,6 @@ pub fn export(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let output = &sig.output;
     let block = &input.block;
 
-    // error if vis is not pub
     if !matches!(vis, syn::Visibility::Public(_)) {
         return syn::Error::new_spanned(vis, "exported function must be public")
             .to_compile_error()
@@ -93,16 +90,28 @@ fn is_usize(ty: &syn::Type) -> bool {
     false
 }
 
-/// #[opaque] or #[opaque(rename="...")] informs plbindgen that the struct is an opaque pointer.
-/// By default the name of the struct will be used for the platypus type alias, but you can rename it.
-#[proc_macro_attribute]
-pub fn opaque(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as syn::ItemStruct);
+/// #[opaque] informs plbindgen that the struct or type alias is an opaque pointer.
+/// the name of the struct will be used for the platypus type alias.
 
-    let vis = &input.vis;
-    let ident = &input.ident;
-    let fields = &input.fields;
-    let semi_token = &input.semi_token;
+#[proc_macro_attribute]
+pub fn opaque(attr: TokenStream, input: TokenStream) -> TokenStream {
+    let item = parse_macro_input!(input as Item);
+
+    match item {
+        Item::Struct(item) => opaque_struct(attr, item),
+        Item::Type(item) => opaque_type(attr, item),
+        _ => syn::Error::new_spanned(item, "#[opaque] can only be used on structs or type aliases")
+            .to_compile_error()
+            .into(),
+    }
+}
+
+
+fn opaque_struct(_attr: TokenStream, item: ItemStruct) -> TokenStream {
+    let vis = &item.vis;
+    let ident = &item.ident;
+    let fields = &item.fields;
+    let semi_token = &item.semi_token;
 
     if !matches!(vis, syn::Visibility::Public(_)) {
         return syn::Error::new_spanned(vis, "#[opaque] struct must be public")
@@ -110,17 +119,27 @@ pub fn opaque(attr: TokenStream, item: TokenStream) -> TokenStream {
             .into();
     }
 
-    if !attr.is_empty() {
-        let rename = syn::parse_macro_input!(attr as syn::MetaNameValue);
-        if !rename.path.is_ident("rename") {
-            return syn::Error::new_spanned(rename, "expected #[opaque(rename = \"...\")]")
-                .to_compile_error()
-                .into();
-        }
-    };
-
     let expanded = quote! {
         pub struct #ident #fields #semi_token
+    };
+
+    TokenStream::from(expanded)
+}
+
+fn opaque_type(_attr: TokenStream, item: ItemType) -> TokenStream {
+    let vis = &item.vis;
+    let ident = &item.ident;
+    let ty = &item.ty;
+    let semi_token = &item.semi_token;
+
+    if !matches!(vis, syn::Visibility::Public(_)) {
+        return syn::Error::new_spanned(vis, "#[opaque] type must be public")
+            .to_compile_error()
+            .into();
+    }
+
+    let expanded = quote! {
+        pub type #ident = #ty #semi_token
     };
 
     TokenStream::from(expanded)
