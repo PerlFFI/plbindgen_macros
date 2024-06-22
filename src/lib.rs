6bd673e5898fa6a5bacdc91913d67655e2ac4e27
Dylan Hardison <dylan@hardison.net>
2024-06-21 21:28:53 -0700

    refine what these macros do

--- src/lib.rs
+++ src/lib.rs
@@ -2,67 +2,32 @@ use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::{parse_macro_input, FnArg, ItemFn, TypeReference, TypeSlice};
 
-/// the plbindgen macro is a procedural macro that will generate a C-compatible function,
-/// and support semi-automatic conversion of &[T] to *const T and usize.
-/// 
-/// it is probably most useful in combination with plbindgen.
+/// the plbindgen macro is a procedural macro that will generate a C-compatible function
+/// and signals to plbindgen that it may produce FFI::Platypus bindings for it.
 #[proc_macro_attribute]
 pub fn export(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemFn);
 
     // let _args = parse_macro_input!(attr as AttributeArgs);
 
+    let vis = &input.vis;
     let sig = &input.sig;
     let unsafety = &sig.unsafety;
     let fn_name = &sig.ident;
     let inputs = &sig.inputs;
     let output = &sig.output;
     let block = &input.block;
-    let mut defs = vec![];
-    let mut new_inputs = Vec::new();
 
-    // Because rust-analyzer does not like adding new secret arguments,
-    // when we detect an &[T] argument, we will require the next argument is a usize
-    // with a particular name, and then we will construct the slice ourselves.
-    for input in inputs {
-        if let FnArg::Typed(pat_type) = input {
-            let pat = &pat_type.pat;
-            let mut ty = &pat_type.ty;
-            // C FFI can't do slices, but FFI::Platypus can do arrays.
-            // If we see a name: &[T], then let's do name: *const T, name_len: usize
-            // and then construct our own slice with the same name
-            let mut is_slice = false;
-            if let syn::Type::Reference(TypeReference { elem, .. }) = ty.as_ref() {
-                if let syn::Type::Slice(TypeSlice { elem, .. }) = elem.as_ref() {
-                    ty = elem;
-                    is_slice = true;
-                }
-            }
-
-            if is_slice {
-                let ident = match pat.as_ref() {
-                    syn::Pat::Ident(ident) => ident.ident.to_string(),
-                    _ => panic!("Expected identifier"),
-                };
-                let pat_len = format_ident!("{}_len", ident);
-                new_inputs.push(quote! {
-                    #pat: *const #ty
-                });
-                defs.push(quote! ( 
-                    let #pat = unsafe { std::slice::from_raw_parts(#pat, #pat_len) };
-                 ));
-            } else {
-                new_inputs.push(quote! {
-                    #pat: #ty
-                });
-            }
-        }
+    // error if vis is not pub
+    if !matches!(vis, syn::Visibility::Public(_)) {
+        return syn::Error::new_spanned(vis, "exported function must be public")
+            .to_compile_error()
+            .into();
     }
 
     let expanded = quote! {
         #[no_mangle]
-        pub #unsafety extern "C" fn #fn_name(#(#new_inputs),*) #output {
-            #(#defs)*
+        pub #unsafety extern "C" fn #fn_name(#inputs) #output {
             #block
         }
     };
@@ -70,27 +35,29 @@ pub fn export(_attr: TokenStream, item: TokenStream) -> TokenStream {
     TokenStream::from(expanded)
 }
 
-#[proc_macro_attribute]
-pub fn opaque(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as syn::ItemStruct);
-
-    let ident = &input.ident;
-    let fields = &input.fields;
-    let semi_token = &input.semi_token;
-    let expanded = quote! {
-        pub struct #ident #fields #semi_token
-    };
-
-    TokenStream::from(expanded)
-}
-
+/// the record macro is a procedural macro that will generate a C-compatible struct,
+/// and signals to plbindgen that it may produce FFI::Platypus::Record bindings for it.
 #[proc_macro_attribute]
 pub fn record(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as syn::ItemStruct);
 
+    let vis = &input.vis;
     let ident = &input.ident;
     let fields = &input.fields;
     let semi_token = &input.semi_token;
+
+    if !matches!(vis, syn::Visibility::Public(_)) {
+        return syn::Error::new_spanned(vis, "#[record] struct must be public")
+            .to_compile_error()
+            .into();
+    }
+    
+    if !matches!(fields, syn::Fields::Named(_)) {
+        return syn::Error::new_spanned(fields, "#[record] struct must have named fields")
+            .to_compile_error()
+            .into();
+    }
+
     let expanded = quote! {
         #[repr(C)]
         pub struct #ident #fields #semi_token
--- tests/basic.rs
+++ tests/basic.rs
@@ -1,18 +1,13 @@
-
 #[cfg(test)]
 mod tests {
+    use plbindgen_macros::*;
 
-    #[plbindgen_macros::opaque]
-    struct Point {
-        x: i32,
-        y: i32,
-    }
+    #[allow(non_camel_case_types)]
+    type array<T> = *const T;
 
-    #[plbindgen_macros::opaque]
-    struct Window(i32, i32, i32, i32);
-
-    #[plbindgen_macros::export]
-    unsafe fn avg(nums: &[i32], nums_len: usize) -> i32 {
+    #[export]
+    pub unsafe fn avg(nums: array<i32>, nums_len: usize) -> i32 {
+        let nums = std::slice::from_raw_parts(nums, nums_len);
         let sum: i32 = nums.iter().sum();
         let avg = sum / nums.len() as i32;
         println!("Average: {}", avg);
@@ -27,20 +22,5 @@ mod tests {
         assert_eq!(avg, 3);
     }
 
-    #[test]
-    fn test_point() {
-        let point = Point { x: 1, y: 2 };
-        assert_eq!(point.x, 1);
-        assert_eq!(point.y, 2);
-    }
-
-    #[test]
-    fn test_window() {
-        let window = Window(1, 2, 3, 4);
-        assert_eq!(window.0, 1);
-        assert_eq!(window.1, 2);
-        assert_eq!(window.2, 3);
-        assert_eq!(window.3, 4);
-    }
 
 }
